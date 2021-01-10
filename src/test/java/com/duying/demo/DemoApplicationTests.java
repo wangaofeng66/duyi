0331b0adba748464821b151c7af60ba0c019ffe5
git <1311748487@qq.com>
2021-01-10 17:11:51 +0800

    first

---
+++ src/test/java/com/duying/demo/DemoApplicationTests.java
@@ -0,0 +1,20 @@
+package com.duying.demo;
+
+import com.google.gson.Gson;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        String jsonStr = "{\"hello\":\"你好\"}";
+        Gson gson = new Gson();
+        Map map = gson.fromJson(jsonStr, Map.class);
+        System.out.println(map);
+    }
+
+}
